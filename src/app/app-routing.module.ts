c42848a4ba899b11d4c125749f657daf1ae897aa
DESKTOP-CU7PFRA\VNPT <havantruong@vnpt.vn>
2022-09-09 15:34:23 +0700

    Nền tảng template appstore

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", pathMatch: "full", redirectTo: "home" },
+  {
+    path: "home",
+    pathMatch: "prefix",
+    component: HomeComponent,
+    //canActivate: [AuthGuard],
+    children: [
+      {
+        path: "",
+        component: HomeComponent,
+        data: { title: 'Dashboard' }
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
