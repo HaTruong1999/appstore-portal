ced7163c4b65a009266681f9f05311d7bab70b43
DESKTOP-CU7PFRA\VNPT <havantruong@vnpt.vn>
2022-10-03 09:22:13 +0700

    menu mobile responsive

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+declare let $: any;
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -10,6 +10,16 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    if($('.menu-trigger').length){
+      $(".menu-trigger").on('click', function() { 
+        $(".menu-trigger").toggleClass('active');
+        $('.header-area .nav').slideToggle(200);
+      });
+
+      $(".nav .scroll-to-section").on('click', function() { 
+        $('.header-area .nav').hide(200);
+      });
+    }
   }
 
 }
