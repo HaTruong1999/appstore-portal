a1d2e2eb7219c1f062c8b78d5f9585025e9a9d7d
DESKTOP-CU7PFRA\VNPT <havantruong@vnpt.vn>
2022-10-17 10:15:00 +0700

    add env and service

---
+++ src/app/core/constant.ts
@@ -0,0 +1,66 @@
+
+import { environment } from '../../environments/environment';
+
+export const Constant = {
+    //auth
+    AUTH_USER_LOGIN: 'auth/login',
+    AUTH_USER_CHECK: 'auth/checkToken',
+    AUTH_USER_CHANGE_PASSWORD: 'auth/changePassword',
+    AUTH_USER_INFO: 'auth/account',
+    AUTH_USER_CHANGE_AVATA: 'auth/changeAvatar',
+    AUTH_USER_CHANGE_PROFILE: 'auth/changeProfile',
+
+    //ListMenus
+    Listmenu: 'api/v1/webadmin/listmenus',
+
+    //Positions
+    Positions: 'api/v1/webadmin/positions',
+
+    //Customers
+    Customers: 'api/v1/webadmin/customers',
+
+    //TaskRoles
+    TaskRoles: 'api/v1/webadmin/taskroles',
+
+    //Workplaces
+    //Workplaces: 'api/v1/webadmin/workplaces',
+
+    //WorkplaceType
+    WorkplaceType: 'api/v1/webadmin/workplacetype',
+
+    //Roles
+    Roles: 'api/v1/webadmin/roles',
+
+    //Accounts
+    Accounts: 'api/v1/webadmin/accounts',
+
+    //Users
+    //Users: 'api/v1/webadmin/users',
+    Users: 'users',
+
+    //PersonalContacts
+    PersonalContacts: 'api/v1/webadmin/personalcontacts',
+
+    //Functions
+    Functions: 'api/v1/webadmin/functions',
+
+    //FuncMobile
+    FuncMobile: 'api/v1/webadmin/func-mobile',
+
+    //Setting
+    Setting: 'api/v1/webadmin/setting',
+
+    //Meeting
+    Meeting: 'api/v1/webadmin/meeting',
+
+    //Apps
+    Apps: 'apps',
+
+    //Apptypes
+    Apptypes: 'apptypes',
+
+    //Workplaces
+    Workplaces: 'workplaces'
+
+}
+
